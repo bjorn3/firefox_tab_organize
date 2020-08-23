8796160524d6bdf1771180a03927a3ae8285a91b
bjorn3 <bjorn3@users.noreply.github.com>
2020-08-23 18:46:56 +0200

    Incrementalize rebuilding of the windows list

--- popup/action.js
+++ popup/action.js
@@ -6,17 +6,48 @@ window.browser.windows.onFocusChanged.addListener(event_handler(reload_window_li
 function event_handler(handler) {
     return function () {
         clear_messages();
-        handler().catch((err) => show_message(err));
+        handler().catch((err) => {
+            show_message(err);
+            throw err;
+        });
+    }
+}
+
+function update_rendered_list(root_el, tag, prefix, elems, cb) {
+    for (let el of root_el.querySelectorAll("." + prefix)) {
+        if (el.id.indexOf(prefix + "-") != 0 || !elems.some((elem) => elem.id == el.id.substr(prefix + 1))) {
+            console.log(el.id);
+            root_el.removeChild(el);
+        }
+    }
+    for (let elem of elems) {
+        let el = document.getElementById(prefix + "-" + elem.id);
+        if (!el) {
+            el = document.createElement(tag);
+            el.id = prefix + "-" + elem.id;
+            el.classList.add(prefix);
+            root_el.appendChild(el);
+        }
+        cb(elem, el);
     }
 }
 
 async function reload_window_list() {
-    let windows_el = document.querySelector("#windows");
     let window_list = await window.browser.windows.getAll({ populate: true });
     console.log(window_list);
-    let html = "<h2>Windows</h2>";
-    for (let win of window_list) {
-        html += "<details class='window'" + (win.focused ? " style='underline:red;' open='true'" : "") + "><summary>" + win.title + (win.focused ? " (Active)" : "") + "</summary>";
+
+    let windows_el = document.querySelector("#windows");
+    update_rendered_list(windows_el, "details", "window", window_list, (win, win_el) => {
+        for (let el of win_el.childNodes) {
+            win_el.removeChild(el);
+        }
+
+        win_el.open = win.focused;
+        let summary = document.createElement("summary");
+        summary.textContent = win.title + (win.focused ? " (Active)" : "");
+        win_el.appendChild(summary);
+
+        let html = "";
 
         let host_tabs = {};
         for (let tab of win.tabs) {
@@ -38,7 +69,7 @@ async function reload_window_list() {
                 html += "<details><summary>" + host + " (" + tabs.length + ")"+ "</summary>";
             }
             for (let tab of tabs) {
-                html += "<div>";
+                html += "<div id='tab-" + tab.id + " class='tab''>";
                 if (tab.favIconUrl) {
                     html += "<img src='" + tab.favIconUrl + "' width='16' height='16'>";
                 }
@@ -49,9 +80,8 @@ async function reload_window_list() {
                 html += "</details>";
             }
         }
-        html += "</details>"
-    }
-    windows_el.innerHTML = html;
+        win_el.innerHTML += html;
+    });
 }
 
 document.querySelector("#discard").onclick = event_handler(async function () {
