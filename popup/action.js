a25bec4703622a59ea8b1a7473d9571300fdcb13
bjorn3 <bjorn3@users.noreply.github.com>
2020-08-24 10:50:07 +0200

    Use preact

--- popup/action.js
+++ popup/action.js
@@ -1,3 +1,5 @@
+import { h, Component, render } from "../node_modules/preact/dist/preact.mjs";
+
 reload_window_list();
 window.browser.windows.onCreated.addListener(event_handler(reload_window_list));
 window.browser.windows.onRemoved.addListener(event_handler(reload_window_list));
@@ -13,123 +15,91 @@ function event_handler(handler) {
     }
 }
 
-function update_rendered_list(root_el, tag, prefix, elems, cb) {
-    for (let el of root_el.querySelectorAll("." + prefix)) {
-        if (el.id.indexOf(prefix + "-") != 0 || !elems.some((elem) => elem.id == el.id.substr(prefix + 1))) {
-            console.log(el.id);
-            root_el.removeChild(el);
-        }
-    }
-    for (let elem of elems) {
-        let el = document.getElementById(prefix + "-" + elem.id);
-        if (!el) {
-            el = document.createElement(tag);
-            el.id = prefix + "-" + elem.id;
-            el.classList.add(prefix);
-            root_el.appendChild(el);
-        }
-        cb(elem, el);
-    }
-}
-
 async function reload_window_list() {
     let window_list = await window.browser.windows.getAll({ populate: true });
     console.log(window_list);
 
-    let windows_el = document.querySelector("#windows");
-    update_rendered_list(windows_el, "details", "window", window_list, (win, win_el) => {
-        for (let el of win_el.childNodes) {
-            win_el.removeChild(el);
+    let win_els = [];
+    win_els.push(h("h2", {}, "Windows"));
+    for (let win of window_list) {
+        win_els.push(Window(win));
+    }
+    render(win_els, document.getElementById("windows"));
+}
+
+function Window(win) {
+    let win_els = [];
+    win_els.push(h("summary", {}, win.title + (win.focused ? " (Active)" : "")));
+
+    let host_tabs = {};
+    for (let tab of win.tabs) {
+        let host = (new URL(tab.url)).host || "other";
+        if (host_tabs[host] === undefined) {
+            host_tabs[host] = [];
+        }
+        host_tabs[host].push(tab);
+    }
+
+    for (let host in host_tabs) {
+        host_tabs[host] = host_tabs[host].sort((a, b) => a.url > b.url);
+    }
+
+    console.log(host_tabs);
+
+    for (let [host, tabs] of Object.entries(host_tabs).sort((a, b) => a[1].length < b[1].length)) {
+        win_els.push(TabGroup(host, tabs));
+    }
+
+    return h("details", { id: "window-" + win.id, classList: ["window"], open: win.focused }, win_els);
+}
+
+function TabGroup(name, tabs) {
+    if (tabs.length > 1) {
+        let summary_inner_els = [];
+        summary_inner_els.push(name + " (" + tabs.length + ")");
+        summary_inner_els.push(h("div", { classList: ["spacer"] }));
+        summary_inner_els.push(h("button", {
+            onclick: event_handler(function () {
+                return window.browser.tabs.discard(tabs.map((tab) => tab.id));
+            })
+        }, "Discard"));
+        summary_inner_els.push(h("button", {
+            onclick: event_handler(function () {
+                return Promise.all(tabs.map((tab) => window.browser.tabs.reload(tab.id)));
+            })
+        }, "Reload"));
+        let summary_inner = h("div", {}, summary_inner_els);
+        let summary = h("summary", {}, summary_inner);
+
+        let details_els = [summary];
+        for (let tab of tabs) {
+            details_els.push(Tab(tab));
         }
 
-        win_el.open = win.focused;
-        let summary = document.createElement("summary");
-        summary.textContent = win.title + (win.focused ? " (Active)" : "");
-        win_el.appendChild(summary);
+        return h("details", {}, details_els);
+    } else {
+        return Tab(tabs[0]);
+    }
+}
 
-        let host_tabs = {};
-        for (let tab of win.tabs) {
-            let host = (new URL(tab.url)).host || "other";
-            if (host_tabs[host] === undefined) {
-                host_tabs[host] = [];
-            }
-            host_tabs[host].push(tab);
-        }
-
-        for (let host in host_tabs) {
-            host_tabs[host] = host_tabs[host].sort((a, b) => a.url > b.url);
-        }
-
-        console.log(host_tabs);
-
-        for (let [host, tabs] of Object.entries(host_tabs).sort((a, b) => a[1].length < b[1].length)) {
-            let parent_el;
-            if (tabs.length > 1) {
-                parent_el = document.createElement("details");
-                win_el.appendChild(parent_el);
-                let summary = document.createElement("summary");
-                parent_el.appendChild(summary);
-
-                let summary_inner = document.createElement("div");
-                summary.appendChild(summary_inner);
-                summary_inner.textContent = host + " (" + tabs.length + ")";
-
-                let spacer = document.createElement("div");
-                summary_inner.appendChild(spacer);
-                spacer.classList.add("spacer");
-
-                let discard_button = document.createElement("button");
-                summary_inner.appendChild(discard_button);
-                discard_button.textContent = "Discard";
-                discard_button.onclick = event_handler(function () {
-                    return window.browser.tabs.discard(tabs.map((tab) => tab.id));
-                });
-
-                let reload_button = document.createElement("button");
-                summary_inner.appendChild(reload_button);
-                reload_button.textContent = "Reload";
-                reload_button.onclick = event_handler(function () {
-                    return Promise.all(tabs.map((tab) => window.browser.tabs.reload(tab.id)));
-                });
-
-            } else {
-                parent_el = win_el;
-            }
-            for (let tab of tabs) {
-                let div = document.createElement("div");
-                parent_el.appendChild(div);
-                div.id = "tab-" + tab.id;
-                div.classList.add("tab");
-
-                if (tab.favIconUrl) {
-                    let img = new Image(16, 16);
-                    div.appendChild(img);
-                    img.src = tab.favIconUrl;
-                }
-
-                div.appendChild(new Text(tab.url));
-
-                let spacer = document.createElement("div");
-                div.appendChild(spacer);
-                spacer.classList.add("spacer");
-
-                let discard_button = document.createElement("button");
-                div.appendChild(discard_button);
-                discard_button.textContent = "Discard";
-                discard_button.onclick = event_handler(function () {
-                    return window.browser.tabs.discard(tab.id);
-                });
-
-                let reload_button = document.createElement("button");
-                div.appendChild(reload_button);
-                reload_button.textContent = "Reload";
-                reload_button.onclick = event_handler(function () {
-                    return window.browser.tabs.discard(tab.id);
-                });
-
-            }
-        }
-    });
+function Tab(tab) {
+    let els = [];
+    if (tab.favIconUrl) {
+        els.push(h("img", { src: tab.favIconUrl, width: 16, height: 16 }));
+    }
+    els.push(tab.url);
+    els.push(h("div", { classList: ["spacer"] }));
+    els.push(h("button", {
+        onclick: event_handler(function () {
+            return window.browser.tabs.discard(tab.id);
+        })
+    }, "Discard"));
+    els.push(h("button", {
+        onclick: event_handler(function () {
+            return window.browser.tabs.reload(tab.id);
+        })
+    }, "Reload"));
+    return h("div", { id: "tab-" + tab.id, classList: ["tab"] }, els);
 }
 
 document.querySelector("#discard").onclick = event_handler(async function () {
